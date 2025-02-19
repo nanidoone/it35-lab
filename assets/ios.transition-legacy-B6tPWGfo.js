866009d12dae4f923a83c6e77ef3b859c96a3db2
nanidoone <vicariatodoneta@gmail.com>
2025-02-19 22:38:04 +0800

    Updates

--- assets/ios.transition-legacy-B6tPWGfo.js
+++ assets/ios.transition-legacy-B6tPWGfo.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BDxNaEEM.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-B1C92XI9.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/keyboard2-legacy-DLibM5pr.js
+++ assets/keyboard2-legacy-DLibM5pr.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BDxNaEEM.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-B1C92XI9.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
