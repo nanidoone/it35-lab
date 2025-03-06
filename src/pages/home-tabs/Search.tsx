724fb66adcdb84b33e8ee1cce8de8a403f60577e
Nanidoone <vicariatodoneta@gmail.com>
2025-03-07 00:46:00 +0800

    added IonSearchBar

--- src/pages/home-tabs/Search.tsx
+++ src/pages/home-tabs/Search.tsx
@@ -1,39 +1,50 @@
+import React from 'react';
 import { 
     IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar 
-  } from '@ionic/react';
-  
-  const Search: React.FC = () => {
+    IonContent, 
+    IonHeader, 
+    IonMenuButton, 
+    IonPage, 
+    IonTitle, 
+    IonToolbar,
+    IonSearchbar // Import IonSearchbar
+} from '@ionic/react';
+
+const Search: React.FC = () => {
+    const handleSearch = (event: CustomEvent) => {
+        console.log('Search query:', event.detail.value);
+    };
+
     return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Search</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              height: '100%',
-            }}
-          >
-            Search
-          </div>
-  
-        </IonContent>
-      </IonPage>
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonButtons slot='start'>
+                        <IonMenuButton></IonMenuButton>
+                    </IonButtons>
+                    <IonTitle>Search</IonTitle>
+                </IonToolbar>
+                <IonToolbar>
+                    <IonSearchbar 
+                        onIonChange={handleSearch} 
+                        placeholder="Search..."
+                    />
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+                <div
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        height: '100%',
+                    }}
+                >
+                    Search Results
+                </div>
+            </IonContent>
+        </IonPage>
     );
-  };
-  
-  export default Search;
+};
+
+export default Search;
