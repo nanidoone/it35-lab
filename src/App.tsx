452a479c16a3f8fde5b9fe4b106c9c85970a16f9
nanidoone <vicariatoladydoone@gmail.com>
2025-03-24 13:00:44 +0800

    added an route path in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,7 @@ import '@ionic/react/css/palettes/dark.system.css';
 import './theme/variables.css';
 import Login from './pages/Login';
 import Menu from './pages/Menu';
+import Signup from './pages/Signup';
 
 setupIonicReact();
 
@@ -41,6 +42,7 @@ const App: React.FC = () => (
     <IonReactRouter>
       <IonRouterOutlet>
         <Route exact path="/it35-lab" component={Login} />
+        <Route path="/it35-lab/Signup" component={Signup} />
         <Route path="/it35-lab/app" component={Menu} />
       </IonRouterOutlet>
     </IonReactRouter>
