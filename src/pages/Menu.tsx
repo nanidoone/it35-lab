6648c4c35ab9e61b6641ce81a53bb777deed25ed
nanidoone <vicariatodoneta@gmail.com>
2025-04-08 13:15:49 +0800

    rename the editProfilePage

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -24,7 +24,7 @@ import About from './About';
 import Details from './Details';
 import { supabase } from '../utils/supabaseClient';
 import { useState } from 'react';
-import editProfilePage from './EditProfilePage';
+import editProfilePage from './editProfilePage';
 
 
 const Menu: React.FC = () => {
