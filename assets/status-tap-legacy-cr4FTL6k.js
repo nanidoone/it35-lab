e3761978d839da099b67ae4d1e906e9617b84d54
Nanidoone <vicariatodoneta@gmail.com>
2025-04-10 09:05:25 +0800

    Updates

--- assets/status-tap-legacy-cr4FTL6k.js
+++ assets/status-tap-legacy-cr4FTL6k.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-L3z5KmIO.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.a,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-CNmby7ao.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
